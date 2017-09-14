1b97663f466da36dc41d9159b36b13779a72804d
NguyenHuy <nguyenvanhuy2010@daingai.edu.vn>
2017-09-15 01:28:32 +0700

    First commit
    Begin my startup

---
+++ src/main/java/com/huy/domain/Data.java
@@ -0,0 +1,71 @@
+package com.huy.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "data")
+public class Data implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false)
+	private int id;
+
+	@NotEmpty
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "about")
+	private String about;
+
+	public Data() {
+		super();
+	}
+
+	public Data(int id, String name, String about) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.about = about;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
